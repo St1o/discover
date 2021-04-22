b5920f2f3e8f07c3bb42a55734d3901da0e3df49
Stio <gridasova-1967@mail.ru>
2021-04-22 22:04:36 +0300

    Сверстал блок сервис и подключил слик-слайдер

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('.miniMenu').click(function () {
+        if ($('nav ul').css('display') == 'none') {
+            $('nav ul').slideDown();
+        } else {
+            $('nav ul').slideUp();
+        }
+    });
+
+    $('.service-slider').slick({
+        arrows: false,
+        dots: true,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                centerMode: true,
+                centerPadding: '50px',
+            }
+        }]
+    });
+});
